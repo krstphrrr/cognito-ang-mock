ef5e779d4563b0446a8830c9c3124e5d54b0ced8
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-03-01 22:41:41 -0700

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {
+    path: '**',
+    redirectTo: 'signIn',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  cognito: {
+    userPoolId: 'us-west-1_OkJZzVOK7',
+    userPoolWebClientId: '6u12s9dnfr2b7guc5iakinkk5a',
+  },
+};
